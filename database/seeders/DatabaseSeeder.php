cdbe9a0798c280a29e7781d881c4d41c14de6257
moreishi <moreishi@gmail.com>
2026-05-07 22:03:03 +0800

    Add FoodCategorySeeder to DatabaseSeeder and add food categories data
    Co-Authored-By: moreishi <moreishi@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RegionCitySeeder::class,
             CategorySeeder::class,
+            FoodCategorySeeder::class,
             RoleSeeder::class,
         ]);
 
